01771ea7de6c1d814dc3c13f0ff4109aac2238fd
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2019-06-06 11:47:16 -0500

    otro

---
+++ Mile/PhoneTranslator/PhoneTranslator/MainPageTwo.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhoneTranslator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageTwo : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPageTwo()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
---
+++ Mile/PhoneTranslator/PhoneTranslator/SharedResources.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace PhoneTranslator
+{
+    public static class SharedResources
+    {
+        public static Color ButtonBkColor => Color.FromRgb(0xff, 0xa5, 0);
+        public static Color ButtonTextColor => Color.White;
+    }
+}
