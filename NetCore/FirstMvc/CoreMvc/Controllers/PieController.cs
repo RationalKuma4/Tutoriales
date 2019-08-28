b55158de83ed5c430bea2eeae304533b45ffab88
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2019-08-28 09:28:15 -0500

    fgbfgb

---
+++ NetCore/FirstMvc/CoreMvc/Controllers/PieController.cs
@@ -0,0 +1,21 @@
+﻿using CoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvc.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public PieController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
