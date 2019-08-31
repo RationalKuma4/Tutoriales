516a23ce9aaf81d154fe369106d664db718c5c0a
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2019-08-31 12:54:31 -0500

    modeulo 4

---
+++ NetCore/FirstMvc/CoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using CoreMvc.Models;
+using CoreMvc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies()
+                .OrderBy(p => p.Name);
+            var homeViewModel = new HomeViewModel
+            {
+                Title = "Welcome",
+                Pies = pies.ToList()
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ NetCore/FirstMvc/CoreMvc/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreMvc.Models;
+
+namespace CoreMvc.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
