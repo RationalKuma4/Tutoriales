5d315613521b47e2edb8febd7e0d64382b948887
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2019-09-20 23:51:55 -0500

    fgs

---
+++ NetCore/CoreApiFundamentals-master/src/Controllers/CampsController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository campRepository, IMapper mapper)
+        {
+            _campRepository = campRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> GetCamps(bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _campRepository.GetAllCampsAsync(includeTalks);
+                return _mapper.Map<CampModel[]>(results); ;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database errror");
+            }
+        }
+
+        [HttpGet("{moniker:string}")]
+        public ActionResult<CampModel> Get(string moniker)
+        {
+            try
+            {
+                var result = _campRepository.GetSpeakersByMonikerAsync(moniker);
+                if (result == null) return NotFound();
+
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database errror");
+            }
+        }
+
+        [HttpGet("serach")]
+        public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime theDate, bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _campRepository.GetAllCampsByEventDate(theDate, includeTalks);
+                if (!results.Any()) return NotFound();
+
+                return _mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database errror");
+            }
+        }
+    }
+}
---
+++ NetCore/CoreApiFundamentals-master/src/Models/TalkModel.cs
@@ -0,0 +1,23 @@
+﻿namespace CoreCodeCamp.Models
+{
+    public class TalkModel
+    {
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public int Level { get; set; }
+
+        public SpeakerModel Speaker { get; set; }
+    }
+
+    public class SpeakerModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Company { get; set; }
+        public string CompanyUrl { get; set; }
+        public string BlogUrl { get; set; }
+        public string Twitter { get; set; }
+        public string GitHub { get; set; }
+    }
+}
